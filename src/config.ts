44b2a2c29e75120e8cebd408ce0111de1f4f08f7
ShiJianwen <shijianwen95@gmail.com>
2026-02-25 20:10:07 +0800

    feat: 修改配置

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 export const SITE = {
-    website: "https://simple-portfolio.vicbox.dev", // replace this with your deployed domain
+    website: "https://shijianwen.com", // replace this with your deployed domain
     title: "Simple Portfolio",
     description: "A simple but beautiful portfolio created with Astro",
     tags: ["portfolio", "Resume cv", "Astro"],
     ogImage: "/og-image.webp",
     logo: "logo",
-    logoText: "Portfolio",
+    logoText: "",
     lang: "en",
     favicon: "/favicon.png",
     repository: "https://github.com/vito8916/simple-portfolio.git",
@@ -42,37 +42,37 @@ export const ME = {
 export const SOCIALS = [
     {
         name: "Twitter",
-        url: "https://twitter.com/vicbox.dev",
+        url: "https://x.com/JianwenShi",
         icon: "twitter-x-fill",
         show: true
     },
-    {
-        name: "Facebook",
-        url: "https://www.facebook.com/vicbox.dev",
-        icon: "facebook-fill",
-        show: false
-    },
-    {
-        name: "Instagram",
-        url: "https://www.instagram.com/vicbox.dev",
-        icon: "instagram-fill",
-        show: true
-    },
-    {
-        name: "Youtube",
-        url: "https://www.youtube.com/vicbox.dev",
-        icon: "youtube-fill",
-        show: true
-    },
-    {
-        name: "TikTok",
-        url: "https://tiktok.com/vicbox.dev",
-        icon: "tik-fill",
-        show: false
-    },
+    // {
+    //     name: "Facebook",
+    //     url: "https://www.facebook.com/vicbox.dev",
+    //     icon: "facebook-fill",
+    //     show: false
+    // },
+    // {
+    //     name: "Instagram",
+    //     url: "https://www.instagram.com/vicbox.dev",
+    //     icon: "instagram-fill",
+    //     show: true
+    // },
+    // {
+    //     name: "Youtube",
+    //     url: "https://www.youtube.com/vicbox.dev",
+    //     icon: "youtube-fill",
+    //     show: true
+    // },
+    // {
+    //     name: "TikTok",
+    //     url: "https://tiktok.com/vicbox.dev",
+    //     icon: "tik-fill",
+    //     show: false
+    // },
     {
         name: "GitHub",
-        url: "https://github.com/vicbox.dev",
+        url: "https://github.com/ShiJianwen",
         icon: "github-fill",
         show: true
     }
